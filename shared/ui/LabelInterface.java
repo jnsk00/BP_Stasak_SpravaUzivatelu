d709b2ced8fc8c113308780e6041f11aea18ead5
jnsk00 <100359273+jnsk00@users.noreply.github.com>
2025-04-17 22:26:40 +0200

    Delete shared directory

--- shared/ui/LabelInterface.java
+++
@@ -1,8 +0,0 @@
-package cz.Stasak.shared.ui;
-
-public interface LabelInterface {
-    void setText(String text);
-    String getText();
-    void setVisible(boolean value);
-    boolean isVisible();
-}
--- shared/ui/ListViewInterface.java
+++
@@ -1,7 +0,0 @@
-package cz.Stasak.shared.ui;
-
-import java.util.List;
-
-public interface ListViewInterface<T> {
-    List<T> getItems();
-}
